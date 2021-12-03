b0e3d329726e72700a0e340b911a0b934b266917
dhslrl321 <dhslrl321@gmail.com>
2021-12-04 02:27:14 +0900

    #feature aws.yml 생성

--- sources/src/main/java/com/example/sources/awscli/AwsCommandString.java
+++ sources/src/main/java/com/example/sources/awscli/AwsCommandString.java
@@ -1,9 +1,16 @@
 package com.example.sources.awscli;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class AwsCommandString {
 
+    @Value("${aws.ecs.cluster_name}")
     private String CLUSTER_NAME = "KT-20211113-2";
+
+    @Value("${aws.ecs.subnet}")
     private String SUBNET = "subnet-07439ed188223bb3f";
+
+    @Value("${aws.ecs.security_group}")
     private String SECURITY_GROUP = "sg-01a19961262ae4372";
 
     public String getTaskDetailCommand(String taskName) {
